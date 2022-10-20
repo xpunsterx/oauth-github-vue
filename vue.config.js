4856678221b5106ddbf6254b0415571326749dfc
xpunsterx <punsterde@gmail.com>
2022-10-21 02:56:09 +0300

    build

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
-const path = require("path");
+const path = require("path")
+
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: path.resolve(__dirname, "./docs"),
+  publicPath: "https://xpunsterx.github.io/oauth-github-vue/",
 })
