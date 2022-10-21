6c035eb1fa4bca16eb7f29ae4c6ae503fe73769f
xpunsterx <punsterde@gmail.com>
2022-10-21 03:07:35 +0300

    build

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h1>asasdasd</h1>
     <keep-alive exclude="UserView">
       <router-view :key="$route.fullPath" />
     </keep-alive>
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
-const path = require("path")
-
+const path = require("path");
 module.exports = defineConfig({
   transpileDependencies: true,
   outputDir: path.resolve(__dirname, "./docs"),
-  publicPath: "https://xpunsterx.github.io/oauth-github-vue/",
+  publicPath: process.env.NODE_ENV === 'production'
+      ? 'https://xpunsterx.github.io/oauth-github-vue/'
+      : '/'
 })
